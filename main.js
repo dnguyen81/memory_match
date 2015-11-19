decdfd70505d32f0ebaf4b0bef0ec6b8a6e978db
dnguyen81git <davidhuynguyen81@gmail.com>
2015-11-19 12:11:02 -0800

    added randomization for card creation

--- main.js
+++ main.js
@@ -12,17 +12,9 @@ var games_played = 0;
 var new_pics_array = [];
 //card creation
 var new_card_array =[];
+var random_card_array=[];
 var front = [
 
-    "images/Assault.png",
-    "images/CR90a.png",
-    "images/Gladiator.png",
-    "images/isd.png",
-    "images/Mc30c.png",
-    "images/Mc80.png",
-    "images/nebulon.png",
-    "images/Raider.png",
-    "images/Victory.png",
     "images/Assault.png",
     "images/CR90a.png",
     "images/Gladiator.png",
@@ -37,40 +29,35 @@ var front = [
 var back = [
     "images/rebel.png"
 ];
-function card_create(){
-    var i = 0;
-    console.log('card creation clicked');
-    while (i<front){
-        var random_index=Math.floor(Math.random()*(front.length));
-        create_card_container(front[random_index]);
-        i++;
-        front.splice(random_index,1);
-        return random_index;
 
-    }
-
+function randomize_array (){
+  random_card_array.sort(function() {return 0.5 - Math.random()});
+    console.log("This is the randomized array", random_card_array);
 }
 function create_card_container(){
 console.log('create card container clicked');
-   for (var i =0; i<front.length; i++){
+   reset();
+    for (var i =0; i<front.length; i++){
 
         new_card_array[i] = front[i];
 
-        front.splice(i,1);
-       var random_card_array = front.concat(new_card_array);
+     //   front.splice(i,1);
+        random_card_array = front.concat(new_card_array);
        console.log("this is the random array ", random_card_array);
+        randomize_array ();
     }
-    while(i <random_card_array){
-        var random_index=Math.floor(Math.random()*(random_card_array.length));
-        create_card_container(random_card_array[random_index]);
-        i++;
-    }
+    //while(i <random_card_array){
+    //    var random_index=Math.floor(Math.random()*(random_card_array.length));
+    //    create_card_container(random_card_array[random_index]);
+    //    i++;
+    //}
+
     for (var i = 0; i < random_card_array.length; i++){
-    var card_div = $('<div>').addClass('card');
+    var card_div = $('<div>').addClass('card').attr('onclick', 'card_clicked(this)');
     var front_div = $('<div>').addClass('front');
     var back_div = $('<div>').addClass('back');
     var img_front = $('<img>').attr('src', random_card_array[i]);
-    var img_back = $('<img>').addClass('back').attr('src', 'images/rebel.png', 'onclick', 'card_clicked(this)');
+    var img_back = $('<img>').addClass('back').attr('src', 'images/rebel.png');
     var colsm = $('<div>').addClass('col-sm-2');
     front_div.append(img_front);
     back_div.append(img_back);
@@ -82,11 +69,11 @@ console.log('create card container clicked');
 
 }
 function card_clicked(card_container_element) {
-//    var back_element = $(card_container_element).find('.back');
- //   $(back_element).hide('slow');
+    var back_element = $(card_container_element).find('.back');
+    $(back_element).hide('slow');
 
-//    var front_element = $(card_container_element).find('.front img');
-//    second_card_clicked = $(front_element).attr('src');
+    var front_element = $(card_container_element).find('.front img');
+    second_card_clicked = $(front_element).attr('src');
     var attempt;
     $(card_container_element).addClass('clicked');
     if (first_card_clicked == null) {
@@ -148,6 +135,7 @@ function reset() {
     matches = 0;
     match_counter = 0;
     $('.back').show();
+    $('.col-sm-2').remove();
 }
 
 $(document).ready(function () {
