4e5feec1c0f14ff0cc2bb613247ef161bceb0114
dnguyen81git <davidhuynguyen81@gmail.com>
2015-11-19 22:42:07 -0800

    multiple difficulties added

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var first_card_clicked = null;
 var second_card_clicked = null;
 var card_container_one = null;
 var card_container_two = null;
-var total_possible_matches = 9;
+var total_possible_matches;
 var match_counter = 0;
 var matches = 0;
 var attempts = 0;
@@ -14,9 +14,11 @@ var games_played = 0;
 //=============theme music integration==========//
 var theme_music = true;
 
-//card creation
+//============card creation arrays====================//
 var new_card_array = [];
 var random_card_array = [];
+
+//============original image array==============//
 var front = [
 
     "images/Assault.png",
@@ -30,6 +32,7 @@ var front = [
     "images/Victory.png"
 
 ];
+//=======multiple back cards that randomize during game creation==========//
 var back = [
     "images/rebel2.png",
     "images/imperial2.png",
@@ -53,20 +56,99 @@ function randomize_array() {
     });
     console.log("This is the randomized array", random_card_array);
 }
-function create_card_container() {
+//==========Easy board creation, 6 cards to match ============//
+function easy(){
+    total_possible_matches = 3;
+    reset_stats();
+
     var gamesplayed = ++games_played;
     $('.games-played .value').html(gamesplayed);
     console.log('create card container clicked');
-    start.play();
+    game_start.play();
     $('.col-sm-2').remove();
     display_stats();
-    for (var i = 0; i < front.length; i++) {
-
-        new_card_array[i] = front[i];
+    attempts = 0;
+    front.splice(0,6);
+        new_card_array = front;
         random_card_array = front.concat(new_card_array);
         console.log("this is the random array ", random_card_array);
         randomize_array();
+
+    randomize_back();
+    for (var i = 0; i < random_card_array.length; i++) {
+        var card_div = $('<div>').addClass('card').attr('onclick', 'card_clicked(this)');
+        var front_div = $('<div>').addClass('front');
+        var back_div = $('<div>').addClass('back');
+        var img_front = $('<img>').attr('src', random_card_array[i]);
+        var img_back = $('<img>').addClass('back').attr('src', random_back);
+        var colsm = $('<div>').addClass('col-sm-2');
+        front_div.append(img_front);
+        back_div.append(img_back);
+        card_div.append(front_div);
+        card_div.append(back_div);
+        colsm.append(card_div);
+        $('#game-area').find('.row1').append(colsm);
     }
+}
+
+//===========medium board creation with six matches=======////////////
+function medium(){
+    total_possible_matches = 6;
+        reset_stats();
+
+    var gamesplayed = ++games_played;
+    $('.games-played .value').html(gamesplayed);
+    console.log('create card container clicked');
+    game_start.play();
+    $('.col-sm-2').remove();
+    display_stats();
+    attempts = 0;
+    front.splice(0,3);
+    new_card_array = front;
+    random_card_array = front.concat(new_card_array);
+    console.log("this is the random array ", random_card_array);
+    randomize_array();
+
+    randomize_back();
+    for (var i = 0; i < random_card_array.length; i++) {
+        var card_div = $('<div>').addClass('card').attr('onclick', 'card_clicked(this)');
+        var front_div = $('<div>').addClass('front');
+        var back_div = $('<div>').addClass('back');
+        var img_front = $('<img>').attr('src', random_card_array[i]);
+        var img_back = $('<img>').addClass('back').attr('src', random_back);
+        var colsm = $('<div>').addClass('col-sm-2');
+        front_div.append(img_front);
+        back_div.append(img_back);
+        card_div.append(front_div);
+        card_div.append(back_div);
+        colsm.append(card_div);
+        $('#game-area').find('.row1').append(colsm);
+    }
+}
+
+
+function hard() {
+    total_possible_matches = 9;
+    reset_stats();
+
+    var gamesplayed = ++games_played;
+
+    $('.games-played .value').html(gamesplayed);
+
+    var difficult = $(this).text;
+    console.log('create card container clicked');
+    game_start.play();
+    $('.col-sm-2').remove();
+    display_stats();
+    attempts = 0;
+  //  difficulty(difficult);
+ //   for (var i = 0; i < front.length; i++) {
+
+        new_card_array = front;
+        random_card_array = front.concat(new_card_array);
+        console.log("this is the random array ", random_card_array);
+        randomize_array();
+  //  }
     randomize_back();
     for (var i = 0; i < random_card_array.length; i++) {
         var card_div = $('<div>').addClass('card').attr('onclick', 'card_clicked(this)');
@@ -91,11 +173,7 @@ function card_clicked(card_container_element) {
     second_card_clicked = $(front_element).attr('src');
     var attempt;
     $(card_container_element).addClass('clicked');
-   //if (card_container_one != null && card_container_two != null){
-   //     $('.back').addClass('clicked');
-   //     else {
-   //        $('.back').removeClass('clicked');
-   //    }
+
     if (first_card_clicked == null) {
         card_container_one = card_container_element;
         console.log('this is the 1st card clicked');
@@ -118,16 +196,13 @@ function card_clicked(card_container_element) {
             console.log("matches: " + x);
             attempt = ++attempts;
             $('.attempts .value').html(attempt);
-            // $(back_element).addClass('flipBack');
         } else {
 
-            //      $('.card').removeClass('flipBack');
             console.log('they do not match');
             setTimeout(function () {
                 $(card_container_one).removeClass('clicked', 'flipBack');
                 $(card_container_two).removeClass('clicked', 'flipBack');
                 $('.back').show('fast');
-                //$('.back, second_card_clicked').show();
                 first_card_clicked = null;
                 second_card_clicked = null;
                 attempt = ++attempts;
@@ -144,6 +219,11 @@ function card_clicked(card_container_element) {
 }
 
 function display_stats() {
+    if (total_possible_matches == 3){
+        var acc = (6 / attempts) * 100;
+    }else if (total_possible_matches == 6){
+        var acc = (12 / attempts) * 100;
+    }else
     var acc = (18 / attempts) * 100;
     var accfixed = acc.toFixed(2) + "%";
     console.log(acc);
@@ -153,17 +233,33 @@ function display_stats() {
     } else {
         $('.accuracy .value').html(accfixed);
     }
-}
-function reset() {
+    front = [
+        "images/Assault.png",
+        "images/CR90a.png",
+        "images/Gladiator.png",
+        "images/isd.png",
+        "images/Mc30c.png",
+        "images/Mc80.png",
+        "images/nebulon.png",
+        "images/Raider.png",
+        "images/Victory.png"
+    ];
 
-    display_stats();
-    accuracy = 0;
-    attempts = 0;
+}
+function reset_stats(){
+
+   // attempts = 0;
     matches = 0;
     match_counter = 0;
+}
+function reset_button() {
+
+    display_stats();
+    reset_stats();
     $('.back').show();
     $('.col-sm-2').remove();
-    create_card_container();
+
+    //create_card_container();
 }
 
 $(document).ready(function () {
@@ -185,3 +281,31 @@ $(document).ready(function () {
 
 
 });
+
+//=====another attempt at difficulty, will come back to this later===========//
+//function difficulty(difficult){
+//    switch (difficult) {
+//
+//        case 'Easy':
+//            front.splice(0,6);
+//            new_card_array = front;
+//            random_card_array = front.concat(new_card_array);
+//            console.log("this is the random array ", random_card_array);
+//            randomize_array();
+//            break;
+//        case 'Medium':
+//            front.splice(0,3);
+//            new_card_array = front;
+//            random_card_array = front.concat(new_card_array);
+//            console.log("this is the random array ", random_card_array);
+//            randomize_array();
+//            break;
+//        case 'Hard':
+//            new_card_array = front;
+//            random_card_array = front.concat(new_card_array);
+//            console.log("this is the random array ", random_card_array);
+//            randomize_array();
+//            break;
+//
+//    }
+//}
